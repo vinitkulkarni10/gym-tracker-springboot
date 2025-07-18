c2df7d4d970eed6234861923ad7e63644fd77ada
vinit kulkarni <kulkarnivinit10@gmail.com>
2025-07-19 00:47:33 +0530

    Initial commit for gym tracker project

---
+++ src/main/java/com/vinit/gym/gymtracker/GymtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vinit.gym.gymtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtrackerApplication.class, args);
+	}
+
+}
